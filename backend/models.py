b6e1947310d1733988385504d98fb599a449559e
Sina Roydel <3inaroydl@gmail.com>
2024-02-05 11:16:08 +0330

    create comment model

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
+from django.core.validators import MinValueValidator , MaxValueValidator
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.contrib.auth import get_user_model
 
 class Item(models.Model):
     name = models.CharField(max_length=30)
@@ -8,3 +9,9 @@ class Item(models.Model):
     price = models.FloatField(validators=[MinValueValidator(0)])
 
 
+
+class Comment(models.Model):
+    item = models.ForeignKey(Item,on_delete=models.CASCADE,related_name="comments")
+    user = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,related_name="comments")
+    text = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
